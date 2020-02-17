2a63842a010a9c2d3f9e2c5ddc3bea8d133603cd
Lucas Kuroda <lucas.kuroda@ikatec.com.br>
2020-02-17 17:34:15 -0300

    Increment/decrement - Input com array

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const [state, setState] = React.useState(0)
+  const [textName, setTextName] = React.useState("")
+  const [UmArray, setUmArray] = React.useState([])
+  // console.log(UmArray)
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // UmArray.push(textName)
+    setUmArray([...UmArray,textName])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setState(state + 1)}>increment</button>
+      <button onClick={() => setState(state - 1)}>decrement</button>
+      <h1>{state}</h1>
+
+      <form class="form" onSubmit={handleSubmit}>
+        <h1> Fill out the form below </h1>
+        <div class="field">
+          <label> Nome </label>
+          <input type="text" id="name" name="name" placeholder="DIGITE O NOME" required onChange={(event) => {
+            // console.log(event.target.value)
+            setTextName(event.target.value)
+          }} ></input>
+          <button type="submit" >Adicionar Item</button>
+        </div>
+      </form>
+      <h2>{UmArray.map(items => <li>{items}</li>)}</h2>
     </div>
   );
-}
+};
 
 export default App;
